6dd194937dbd87395aa0c3f6bf5f63a1da3a2c9b
Precious <preciousmaregwa1997@gmail.com>
2022-04-12 10:43:57 +0300

    complete assigment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+const thankYouMessages = [];
+const writeCards = (names,event) => {
+  for (let i = 0; i < names.length; i++){
+    let message = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+    thankYouMessages.push(message);
+  }
+  return thankYouMessages;
+}
+
+const countDown = (value)=>{
+  let i = value;
+  while(i>=0){
+    console.log(i);
+    i--
+  }
+}
